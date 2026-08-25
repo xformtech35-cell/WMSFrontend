f1d725a31ae64cadd6df2604c67d62f2521ca2e9
Kalpesh Hire <kalpesh.hire@xform.in>
2026-08-25 18:51:51 +0530

    all confirmation

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -276,6 +276,14 @@ const ALL_MENU_ITEMS = [
     liveKey: "pendingPicks",
   },
   {
+    href: "/all-confimration",
+    label: "All Confirmation",
+    icon: ScanLine,
+    group: "Fulfillment",
+    permission: P.PICKING_VIEW,
+    liveKey: "pendingPicks",
+  },
+  {
     href: "/trolleys",
     label: "Trolleys",
     icon: Truck,
