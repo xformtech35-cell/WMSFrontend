de4cfa98f790b9be460e679adbe6a3e68d92512a
Kalpesh Hire <kalpesh.hire@xform.in>
2026-09-11 10:50:23 +0530

    search remove

--- src/app/CreatePick/page.jsx
+++ src/app/CreatePick/page.jsx
@@ -543,14 +543,14 @@ export default function SalesOrderPage() {
                             </button>
                           {/* )} */}
                           {/* {(so.status === "DRAFT" || so.status === "PROCESSING") && ( */}
-                            <button
+                            {/* <button
                               type="button"
                               onClick={() => handleDelete(so.soNumber)}
                               className="text-red-600 hover:text-red-800 transition-colors"
                               title="Delete"
                             >
                               <XCircle className="w-4 h-4" />
-                            </button>
+                            </button> */}
                           {/* )} */}
                         </div>
                       </td>
